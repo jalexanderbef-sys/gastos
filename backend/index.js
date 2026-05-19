509640b475c523b2de6c730b0f68927a06f3564b
Stiven Ararat <ambuila@gmail.com>
2026-05-19 09:47:20 -0500

    se creo el index.js

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.listen(3000, ()=> {
+    console.log('cree un servidor web')
+})
+
+app.get('/finanzas',(req,res)=>{
+    res.send('<h1>Hola soy una app de finanzas</h1>')
+})
