b14f8a3633c2ba568129593f0b18b2cbbccfe23a
Stiven Ararat <ambuila@gmail.com>
2026-06-23 08:10:06 -0500

    se agrego el cors

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -2,9 +2,9 @@ let registro = document.getElementById('transacciones');
 fetch ('http://localhost:3000/api/gastos').then(datos => datos.json()).then(registro =>{
 
     for(i=0;i<=5;i++){
-    registro.innerHTML += `<div class="fila ">
-                            <div>Ingreso</div>
-                            <div>20.000</div>
-                        </div>`
+        registro.innerHTML += `<div class="fila ">
+        <div>${registro.data[i].nombre}</div>
+        <div>${registro.data[i].monto}</div>
+        </div>`
     }
 })
