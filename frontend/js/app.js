ef7521530a55d853aefc07742549f58b04063661
Stiven Ararat <ambuilajhener@gmail.com>
2026-06-24 17:56:04 -0500

    modificaciones app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,18 +1,113 @@
-let transaccion = document.getElementById('transacciones');
-fetch ('http://localhost:3000/api/gastos').then(datos => datos.json()).then(registro =>{
+const transacciones =
+document.getElementById('transacciones');
 
-    for(i=0;i<=5;i++){
+const descripcion =
+document.getElementById('descripcion');
 
-        if(registros.data[i].tipo == 1){
-            registro.innerHTML += `<div class="fila gastos">
-            <div>${registro.data[i].nombre}</div>
-            <div>${registro.data[i].monto}</div>
-        </div>`
+const monto =
+document.getElementById('monto');
 
-         }else{
-            transaccion.innerHTML += `<div class="fila ingresos">
-            <div>${registro.data[i].nombre}</div>
-            <div>${registro.data[i].monto}</div>`
-         }
-    }
-})
+const fecha =
+document.getElementById('fecha');
+
+const categoria =
+document.getElementById('categoria');
+
+const btnIngreso =
+document.getElementById('btnIngreso');
+
+const btnGasto =
+document.getElementById('btnGasto');
+
+cargarDatos();
+
+function cargarDatos() {
+
+    fetch('http://localhost:3000/api/gastos')
+
+    .then(res => res.json())
+
+    .then(data => {
+
+        transacciones.innerHTML = '';
+
+        data.forEach(item => {
+
+            const clase =
+                item.tipo === 'GASTO'
+                ? 'gastos'
+                : 'ingresos';
+
+            transacciones.innerHTML += `
+                <div class="fila ${clase}">
+                    <div>${item.categoria}</div>
+                    <div>$${item.monto}</div>
+                    <div>${item.fecha}</div>
+                </div>
+            `;
+        });
+
+    })
+
+    .catch(error => {
+        console.error(error);
+    });
+}
+
+btnIngreso.addEventListener(
+    'click',
+    () => guardarMovimiento('INGRESO')
+);
+
+btnGasto.addEventListener(
+    'click',
+    () => guardarMovimiento('GASTO')
+);
+
+function guardarMovimiento(tipo) {
+
+    fetch(
+        'http://localhost:3000/api/gastos',
+        {
+
+            method: 'POST',
+
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+            body: JSON.stringify({
+
+                descripcion:
+                    descripcion.value,
+
+                monto:
+                    monto.value,
+
+                fecha:
+                    fecha.value,
+
+                categoria_id:
+                    categoria.value,
+
+                tipo:
+                    tipo
+            })
+        }
+    )
+
+    .then(res => res.json())
+
+    .then(() => {
+
+        descripcion.value = '';
+        monto.value = '';
+        fecha.value = '';
+        categoria.value = '';
+        cargarDatos();
+    })
+
+    .catch(error => {
+        console.error(error);
+    });
+}
